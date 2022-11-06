4607e53f4f6060b3460bc87f1695f71e5cd30f9e
José Dotta <josehenriquedotta61@gmail.com>
2022-11-06 14:48:16 -0300

    Primeiro commit

---
+++ src/main/java/br/com/alura/jose/App.java
@@ -0,0 +1,13 @@
+package br.com.alura.jose;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
